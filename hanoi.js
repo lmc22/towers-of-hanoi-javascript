d1d0ff1d693f27047107da99ac82975972029364
Norris Kwan <norris.kwan@gmail.com>
2017-07-01 14:50:17 -0700

    Add promptMove

--- hanoi.js
+++ hanoi.js
@@ -1,5 +1,35 @@
-class Game {
-  constructor() {
-    this.stacks = [];
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+class Hanoi {
+  constructor(stacks) {
+    // this.stacks = [[3, 2, 1], [], []];
+    this.stacks = stacks;
   }
+
+  promptMove(callback) {
+    // 1. print stacks
+    this.print();
+
+    // 2. ask user to/from
+    reader.question("Select starting stack: ", (res1) => {
+      let startTowerIdx = res1;
+      console.log(`Start from ${res1}`);
+
+      reader.question("Select destination stack: ", (res2) => {
+        let endTowerIdx = res2;
+        console.log(`Start from ${res2}`);
+
+        callback(startTowerIdx, endTowerIdx);
+      });
+    });
+  }
+
 }
+
+let game = new Hanoi([[3, 2, 1], [], []]);
