0a21453f4bddb396e6075aba585fc0f210f61b77
Norris Kwan <norris.kwan@gmail.com>
2017-07-01 14:58:20 -0700

    Test and refactor

--- hanoi.js
+++ hanoi.js
@@ -19,12 +19,11 @@ class Hanoi {
     // 2. ask user to/from
     reader.question("Select starting stack: ", (res1) => {
       let startTowerIdx = res1;
-      console.log(`Start from ${res1}`);
 
       reader.question("Select destination stack: ", (res2) => {
         let endTowerIdx = res2;
+        console.log(`Start from ${res1}`);
         console.log(`Start from ${res2}`);
-
         callback(startTowerIdx, endTowerIdx);
       });
     });
@@ -33,25 +32,14 @@ class Hanoi {
   isValidMove(startTowerIdx, endTowerIdx) {
     //  if the disc in the starting position is smaller than the disc in the
     //  ending position, it is a valid move.
-    console.log(
-      `Starting Tower: ${startTowerIdx}\nEnding Tower: ${endTowerIdx}`
-    );
     const stack = this.stacks,
           startLast = stack[startTowerIdx].length-1,
           endLast = stack[endTowerIdx].length-1;
-
-    // let startTop = (startStack === undefined) ? 0 : startStack,
-    //     endTop = (endStack === undefined) ? 0 : endStack,
-    //     validity = (startTop > endTop);
-    console.log(startLast);
-    console.log(endLast);
 
     let startTop = stack[startTowerIdx][startLast],
         endTop = stack[endTowerIdx][endLast],
         validity = (startTop > endTop) ? false : true;
 
-    console.log(startTop);
-    console.log(endTop);
     console.log(`Valid move? ${validity}`);
     return validity;
   }
@@ -78,10 +66,18 @@ class Hanoi {
     for (let i = 1; i < this.stacks.length; i++) {
       if (this.stacks[i].length === 3) {
         return true;
+      } else {
+        return false;
       }
     }
   }
 
+  run(completionCallback) {
+    this.promptMove( (startIdx, endIdx) => {
+      this.move(startIdx, endIdx);
+    });
+  }
 }
 
 let game = new Hanoi([[3, 2, 1], [], []]);
+game.run();
